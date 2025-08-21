b81419ac7512abc4e8d8c963e6ba9cd5f2415a09
abdullahshahid03 <abdulllahh.shahid@gmail.com>
2025-08-22 02:55:50 +0500

    heros section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import video1 from '../assets/video1.mp4';
+import video2 from '../assets/video2.mp4';
+function HeroSection() {
+  return (
+    <div className='flex flex-col items-center justify-center mt-6 lg:mt-20'>
+   <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+    VirtualR build tools
+    <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text"> for developers</span>
+   </h1>
+   <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+    Empower your creativity with our powerful tools and resources.
+    Get started with VirtualR today and turn your ideas into reality.
+   </p>
+   <div className='flex justify-center my-10'>
+<a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
+  Get Started for Free
+</a>
+<a href="#" className='py-3 px-4 mx-3 rounded-md border'>
+  learn more
+</a>
+   </div>
+   <div className='flex mt-10 justify-center'>
+    <video autoPlay loop muted className='w-1/2 rounded-lg border border-orange-700 shadow-orange-400 mx-2 my-4'>
+   <source src={video1} type="video/mp4" />
+    Your browser does not support the video tag.
+   </video>
+   <video autoPlay loop muted className='w-1/2 rounded-lg border border-orange-700 shadow-orange-400 mx-2 my-4'>
+   <source src={video2} type="video/mp4" />
+    Your browser does not support the video tag.
+   </video>
+   </div>
+    </div>
+  );
+}
+
+export default HeroSection;
